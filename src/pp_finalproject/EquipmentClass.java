5a0b7859f6b9732260bc0fa9eec5ebbc5d3c95fc
Diogo Gomes Cardoso <diogogomecardoso@gmail.com>
2022-07-01 22:15:10 +0100

    Equipment Class Added

---
+++ src/pp_finalproject/EquipmentClass.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pp_finalproject;
+
+import estgconstroi.Equipment;
+import estgconstroi.enums.EquipmentStatus;
+import estgconstroi.enums.EquipmentType;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class EquipmentClass implements Equipment{
+    private static String Name;
+    private EquipmentType Type;
+    private EquipmentStatus Status;
+
+    @Override
+    public String getName() {
+        return this.Name;
+    }
+
+    @Override
+    public EquipmentType getType() {
+        return this.Type;
+    }
+
+    @Override
+    public EquipmentStatus getStatus() {
+        return this.Status;
+    }
+
+    @Override
+    public void setStatus(EquipmentStatus es) {
+        this.Status = es;
+    }
+
+}
