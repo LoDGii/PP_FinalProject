790ce3aa57ec524aff719d4a1eccd0a01f1823d2
MrCarrot6 <41472621+MrCarrot6@users.noreply.github.com>
2022-07-04 12:28:06 +0100

    progresso

---
+++ src/pp_finalproject/AccidentClass.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pp_finalproject;
+
+import estgconstroi.Accident;
+import estgconstroi.Employee;
+
+
+public class AccidentClass implements Accident{
+
+    private Employee employee;
+    private String details;
+    private String notification;
+    
+    
+    
+    
+    @Override
+    public Employee getEmployee() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getDetails() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getNotificationMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
